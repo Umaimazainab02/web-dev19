6891c543f9d6e4502d55119459e204ce06523bf1
Umaimazainab02 <umaimazainab429@gmail.com>
2026-05-08 09:42:57 +0500

    first upload

---
+++ script.js
@@ -0,0 +1,24 @@
+// alert("Hello JavaScript!");
+// console.log("Hello JS");
+// console.log("Console me");
+
+function sayHello() {
+    alert("jhjjhj")
+};
+const accId = 12345;
+let accHolderName = "ali";
+let address
+// console.log(address)
+address = "ehsjksfksdjfkskjd"
+
+/*
+number
+string
+boolean
+undefined
+null
+
+*/
+
+console.log(Date.now());
+
