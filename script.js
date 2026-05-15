47cb03866ced949c7e702c6714e244ff64d0020c
Umaimazainab02 <umaimazainab429@gmail.com>
2026-05-15 11:46:06 +0500

    update

--- script.js
+++ script.js
@@ -2,12 +2,12 @@
 // console.log("Hello JS");
 // console.log("Console me");
 
-function sayHello() {
-    alert("jhjjhj")
-};
-const accId = 12345;
-let accHolderName = "ali";
-let address
+//function sayHello() {
+  //  alert("jhjjhj")
+//};
+//const accId = 12345;
+//let accHolderName = "ali";
+//let address
 // console.log(address)
 address = "ehsjksfksdjfkskjd"
 
@@ -20,5 +20,16 @@ null
 
 */
 
-console.log(Date.now());
+//console.log(Date.now());
 
+//QUESTion 01
+let x = "ali"
+let y = 30
+x = x + y
+console.log(x)
+
+//Question 02
+//let z = "ali"
+console.log(typeof x)
+  
+//Question 03
